b29c3b9a666ad3b9c42014b3c9d29e1e6963dcfb
Naveen Micheal <naveenmicheal@protonmail.com>
2020-02-29 00:00:36 +0530

    001 and 002 complete

---
+++ 001-c-side-auth/main.js
@@ -0,0 +1,12 @@
+function auth() {
+    let name = document.getElementById('name').value;
+    let pass = document.getElementById('pass').value;
+    // console.log(name)
+    if(name =="admin" && pass == "letmein"){
+      window.location = "fBndc2d!QRv62=zF.html"
+    }
+    else{
+      alert("WRONG PASSWORD")
+    }
+
+  }
---
+++ 002-i-agree-whtever/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = '112345678'
+TEMPLATES_AUTO_RELOAD = True
+
+@app.route('/')
+def hello_world():
+    return render_template('login.html')
+
+@app.route('/flagpage')
+def flagpage():
+	# print(request)
+	data = request.cookies.get("Admin")
+	if data == 'True':
+		return render_template('flag.html',flag = """mzctf{"AcC3p7-CoOK135-whA73v3R"}""")
+	else:
+		return render_template('flag.html', flag = 'mmmm But Looks Like You are not a Admin!')
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='3333', debug=True)
