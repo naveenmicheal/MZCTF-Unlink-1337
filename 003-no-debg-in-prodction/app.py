9c67d5dd18fcb70c204bd0d5394f4fb977cab546
Naveen Micheal <naveenmicheal@protonmail.com>
2020-03-02 18:47:43 +0530

    003 debug completed, challenge interface

--- 003-no-debg-in-prodction/app.py
+++ 003-no-debg-in-prodction/app.py
@@ -1,38 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, Response, request,make_response, redirect, url_for
 import os
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '112345678'
-app.config['FLASK_DEBUG'] = '111-222-333'
-# app.secret_key = '4444444'
-os.getenv('FLASK_DEBUG')
+os.environ['WERKZEUG_DEBUG_PIN'] = '1337-7331'
 @app.route('/')
 def hello_world():
-	print(dir(app))
-	print('+++++++++++++++++++++++++++++++++++++++++++++++++++')
-	print(app.env)
-	print('+++++++++++++++++++++++++++++++++++++++++++++++++++')
-	print(app.debug)
-	print('/n')
-	# print(app.default_config)
-	print('/n')
-	print(app.config)
-	raise Exception()
-    # return render_template('flag.html')
+	resp = make_response(render_template('flag.html'), 200)
+	# resp.headers.extend({'X-Powered-By': 'AT-5000'})
 
-# @app.route('/flagpage')
-# def flagpage():
-# 	flag = "here"
-# 	data = requst.cookies.get("Admin")
-# 	if data == 'True':
-# 		return render_template('flag.html',flag = """mzctf{"AcC3p7-CoOK135-whA73v3R"}""")
-# 	else:
-# 		return render_template('flag.html', flag = 'mmmm But Looks Like You are not a Admin!')
+	resp.headers.add_header("Debug-Pin","1337-7331")
+	resp.headers.add_header("Shell-Page-Location","/shell")
+	# raise Exception()
+	return resp
+
+@app.route('/shell')
+def do():
+	raise Exception()
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='3333', debug=True)
 
 
-
+# 
 # https://help.semmle.com/wiki/display/PYTHON/Flask+app+is+run+in+debug+mode
